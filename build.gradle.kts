b89d6efb92b380b652a344fa225f366eb9583f8e
GitHub Action <action@github.com>
2026-08-07 14:54:28 +0000

    chore(release): version 0.5.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ repositories {
 }
 
 group = "io.github.yellowhammer"
-version = "0.4.1"
+version = "0.5.0"
 
 // Корень XSD: каталог submodule `resources/namespace-forest/` (см. .gitmodules, gradle.properties `xsd.root`).
 val xsdRootPath = (findProperty("xsd.root") as String?) ?: "resources/namespace-forest"
