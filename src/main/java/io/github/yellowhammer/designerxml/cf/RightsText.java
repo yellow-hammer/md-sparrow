41cb3b69a0afa40e49a01e18b73c6d8c69277537
Ivan Karlo <i.karlo@outlook.com>
2026-09-26 21:33:37 +0300

    chore(license): привели заголовки исходников к полному тексту license/HEADER.txt
    strictCheck принимает только полный заголовок. Два файла были без него
    (EmptyCfeScaffold, WriteProbeTest), у 225 стояли укороченные шапки со
    строкой SPDX: 131 с копирайтом и 94 без него. Заголовки выставил
    licenseFormat; шапку без слова Copyright плагин заголовком не считает,
    поэтому её остаток под новым заголовком убрали вручную.

--- src/main/java/io/github/yellowhammer/designerxml/cf/RightsText.java
+++ src/main/java/io/github/yellowhammer/designerxml/cf/RightsText.java
@@ -5,6 +5,19 @@
  * Ivan Karlo <i.karlo@outlook.com> and contributors
  *
  * SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ * md-sparrow is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * md-sparrow is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with md-sparrow.
  */
 package io.github.yellowhammer.designerxml.cf;
 
